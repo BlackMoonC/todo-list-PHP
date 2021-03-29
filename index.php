6283e433f1aaa2675d6a0d6ecf31da250443e9cd
BlackMoonC <rakelsera2001@gmail.com>
2021-03-29 23:55:26 +0700

    master

---
+++ index.php
@@ -0,0 +1,23 @@
+<form action="">
+<label>Daftar Pekerjaan Hari Ini</label> </br>
+<input type="text" name="todo">
+<button type="submit">Simpan</button>
+</form>
+
+<ul>
+    <li>
+        <input type="checkbox" name="todo">
+        <label> Todo 1</label>
+        <a href="#">hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name="todo">
+        <label> Todo 1 </label>
+        <a href="'#">hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name="todo">
+        <label> Todo 1 </label>
+        <a href="'#">hapus</a>
+    </li>
+</ul>
