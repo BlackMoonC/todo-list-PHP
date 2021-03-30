465625615404906f7574f5c2e63788dc27899d0c
BlackMoonC <rakelsera2001@gmail.com>
2021-03-30 12:47:07 +0700

    master

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+?>
+
+
 <form action="">
 <label>Daftar Pekerjaan Hari Ini</label> </br>
 <input type="text" name="todo">
@@ -5,19 +10,12 @@
 </form>
 
 <ul>
+<?php foreach ($variable as $value) {
+ ?>
     <li>
         <input type="checkbox" name="todo">
         <label> Todo 1</label>
         <a href="#">hapus</a>
     </li>
-    <li>
-        <input type="checkbox" name="todo">
-        <label> Todo 1 </label>
-        <a href="'#">hapus</a>
-    </li>
-    <li>
-        <input type="checkbox" name="todo">
-        <label> Todo 1 </label>
-        <a href="'#">hapus</a>
-    </li>
+<?php  }?>
 </ul>
